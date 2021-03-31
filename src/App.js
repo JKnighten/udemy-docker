fa2e26628870220d8bd96f46861ab39d4906581f
Johnny K <jknigh28@gmail.com>
2021-03-30 19:20:39 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EDIT <code>src/App.js</code> and save to reload.
+         Changed on feature brnach
         </p>
         <a
           className="App-link"
